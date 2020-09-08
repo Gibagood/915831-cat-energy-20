a4db79e86375f9aea33f109808fb4dad853df4a9
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-09-08 07:33:00 +0300

    слайдер js

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,5 +1,6 @@
 var beforeImg = document.querySelector('.example__before-img');
 var scale = document.querySelector('.example__scale-input');
+var scale-value = document.querySelector('.example__scale-value');
 /* 
 if (beforeImg && scale) {
     beforeImg.style.width = '75%';
@@ -7,5 +8,6 @@ if (beforeImg && scale) {
 }
  */
 scale.oninput = function () {
-
+    scale-value.textContent = scale.value;
+    beforeImg.style.width = scale.value + '%';
 };
