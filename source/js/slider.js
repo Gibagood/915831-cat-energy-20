5b31490d3951a965b1ff20ded7ba3f701cdcac3d
Gibagood <fotogibadullin@gmail.com>
2020-09-08 15:28:57 +0300

    ретиновые изображения и недоделанный слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,12 +2,7 @@ var beforeImg = document.querySelector('.example__before-img');
 var afterImg = document.querySelector('.example__after-img');
 var scale = document.querySelector('.example__scale-input');
 var scaleValue = document.querySelector('.example__scale-value');
-/* 
-if (beforeImg && scale) {
-    beforeImg.style.width = '75%';
-    scale.style.width = '65%'
-}
- */
+
 scale.oninput = function () {
     scaleValue.textContent = scale.value;
     scale.style.width = scale.value + '%';
