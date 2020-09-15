4955a7b170ff266d7b63d154aea2f7fdaa0a77d0
Gibagood <63713936+Gibagood@users.noreply.github.com>
2020-09-16 00:45:55 +0300

    добавляет ховеры и активы к svg и другим

--- source/js/invalid.js
+++ source/js/invalid.js
@@ -1,5 +1,6 @@
 const formButton = document.querySelector(".form__button");
 const inputs = document.querySelectorAll(".validate-input");
+const images = document.querySelectorAll(".validate-image");
 
 formButton.addEventListener("click", function (evt) {
   for (let i = 0; i < inputs.length; i++) {
@@ -8,10 +9,23 @@ formButton.addEventListener("click", function (evt) {
       inputs[i].classList.add("validate-input--invalid");
     }
   }
+
+  for (let j = 0; j < images.length; j++) {
+    if (!images[j].value) {
+      evt.preventDefault();
+      images[j].classList.add("validate-image--invalid");
+    }
+  }
 });
 
 for (let i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener('click', function() {
     this.classList.remove("validate-input--invalid")
+  });
+}
+
+for (let j = 0; j < images.length; j++) {
+  images[j].addEventListener('click', function() {
+    this.classList.remove("validate-images--invalid")
   });
 }
